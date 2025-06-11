7019d273a4b2237422761963bd34ca78c02ef8f1
Douglas Dias Leal <douglas_san@hotmail.com>
2025-06-12 07:23:15 +0900

    feat: add input and button for project creation in Hero component

--- app/components/landing-page/hero.tsx
+++ app/components/landing-page/hero.tsx
@@ -7,6 +7,27 @@ export default function Hero() {
           Crie sua própria página de projetos e compartilhe eles com o mundo.
           <br />
           Acompanhe o engajamento com Analytics de cliques</h2>
+          <div className="flex items-center gap-2 w-full -mt-[10vh]">
+            <span className="text-white text-xl">projectinbio.com</span>
+            {/* TODO component */}
+            <input type="text" />
+            <button>Criar agora</button>
+          </div>
+          <div className="w-full flex items-center justify-center bg-[radial-gradient(circle_at_50%_50%,#4B2DBB, transparent_55%)]">
+            <div className="relative">
+              {/* <UserCard /> */}
+              <div className="absolute -bottom-[7%] -right-[45%]">
+                {/* <TotalVisits /> */}
+              </div>
+              <div className="absolute -top-[20%] -left-[45%] -z-10">
+                {/* <ProjectCard /> */}
+              </div>
+              <div className="absolute -top-[5%] -left-[55%] -z-10">
+                {/* <ProjectCard /> */}
+              </div>
+            </div>
+
+          </div>
       </div>
     </header>
   )
