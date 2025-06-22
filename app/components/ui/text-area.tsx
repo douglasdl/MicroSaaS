fba73a6b654cd4c175bca4706e07592475b90a5c
Douglas Dias Leal <douglas_san@hotmail.com>
2025-06-22 14:34:38 +0900

    feat: enhance New Project modal with input fields for project title, description, and URL, and integrate image upload functionality

--- app/components/ui/text-area.tsx
+++ app/components/ui/text-area.tsx
@@ -8,11 +8,12 @@ interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
 export function TextArea({ children, ...props }: TextAreaProps) {
   return (
     <textarea
+      {...props}
       className={cn(
-        'w-full p-3 bg-background-secondary text-white placeholder:text-content-placeholder rounded-xl border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary',
+        `w-full p-3 bg-background-secondary text-white placeholder:text-content-placeholder rounded-xl 
+      border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary`,
         props.className
       )}
-      {...props}
     >
       {children}
     </textarea>
