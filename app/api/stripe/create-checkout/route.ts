51c1a9fbedbac982d7ebc1a874892a9e6ec40988
Douglas Dias Leal <douglas_san@hotmail.com>
2025-06-29 07:53:51 +0900

    feat: update plan button to initiate Stripe checkout for one-time payment and modify payment method types to include 'konbini'

--- app/api/stripe/create-checkout/route.ts
+++ app/api/stripe/create-checkout/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
       },
     ],
     mode: isSubscription ? 'subscription' : 'payment',
-    payment_method_types: isSubscription ? ['card'] : ['card', 'boleto'],
+    payment_method_types: isSubscription ? ['card'] : ['card', 'konbini'],
     success_url: `${req.headers.get('origin')}/${metadata.profileId}`,
     cancel_url: `${req.headers.get('origin')}/${metadata.profileId}/upgrade`,
     metadata,
