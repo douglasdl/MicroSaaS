6f61400975a0198206edc8dc9b9ba5e10a630d13
Douglas Dias Leal <douglas_san@hotmail.com>
2025-06-30 06:45:49 +0900

    feat: enhance NextAuth configuration to include user creation timestamp and trial status in session, improving user management and experience

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,14 +1,50 @@
 import { FirestoreAdapter } from '@auth/firebase-adapter'
-import NextAuth from 'next-auth'
+import { Timestamp } from 'firebase-admin/firestore'
+import NextAuth, { type DefaultSession } from 'next-auth'
 import Google from 'next-auth/providers/google'
 import { env } from '../env'
-import { firebaseCert } from './firebase'
+import { TRIAL_DAYS } from './config'
+import { db, firebaseCert } from './firebase'
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      createdAt: number
+      isTrial: boolean
+    } & DefaultSession['user']
+  }
+
+  interface User {
+    createdAt: number
+    isTrial?: boolean
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: FirestoreAdapter({
     credential: firebaseCert,
   }),
   providers: [Google],
-  events: {},
-  callbacks: {},
+  events: {
+    createUser: async ({ user }) => {
+      if (!user.id) return
+
+      await db.collection('users').doc(user.id).update({
+        createdAt: Timestamp.now().toMillis(),
+      })
+    },
+  },
+  callbacks: {
+    session({ session, user }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          isTrial:
+            new Date(user.createdAt).getTime() >
+              new Date().getTime() - 1000 * 60 * 60 * 24 * TRIAL_DAYS || false,
+        },
+      }
+    },
+  },
 })
